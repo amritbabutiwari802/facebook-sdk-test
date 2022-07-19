6a3ba215b15817b014f5f0c73dfc390ad9077298
babuamrit <92739498+babuamrit@users.noreply.github.com>
2022-07-19 19:16:03 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
-import React, { Component } from 'react';
+import React, { useEffect } from "react";
 
-import './App.css';
-import FacebookProvider, { Login } from 'react-facebook-sdk';
 
-class App extends Component {
-  handleResponse = (data) => {
-    console.log(data);
-    window.FB.api(
-      `/${data.tokenDetail.userID}/permissions`,
-      "GET",
-      function (permissionResponse) {
-        console.log("granted permission", permissionResponse);
-        window.FB.api(
-          `/${data.tokenDetail.userID}/accounts?fields=name&access_token=${data.tokenDetail.accessToken}`,
-          "GET",
-          async function (assigendPages) {
-            console.log(assigendPages);
+
+const App = (props) => {
+  useEffect(() => {
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: "343921607951637",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v14.0",
+        status: true,
+      });
+    };
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName("script")[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement("script");
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
+  return (
+    <div
+      onClick={() => {
+        window.FB.login(
+          function (response) {
+            if (response.authResponse) {
+              console.log("Welcome!  Fetching your information.... ", response);
+              window.FB.api(
+                `/${response.authResponse.userID}/permissions`,
+                "GET",
+                function (permissionResponse) {
+                  console.log("granted permission", permissionResponse);
+                  window.FB.api(
+                    `/${response.authResponse.userID}/accounts?fields=name&access_token=${response.authResponse.accessToken}`,
+                    "GET",
+                    async function (assigendPages) {
+                      const allPages = assigendPages.data;
+                      console.log(assigendPages);
+                    }
+                  );
+                }
+              );
+            } else {
+              console.log("User cancelled login or did not fully authorize.");
+            }
+          },
+          {
+            scope:
+              "public_profile,email,pages_messaging,pages_read_engagement,pages_show_list,pages_manage_metadata",
           }
         );
-      }
-    );
-  }
- 
-  handleError = (error) => {
-    this.setState({ error });
-  }
-
-  render() {
-    return (
-      <FacebookProvider appId="343921607951637">
-        <Login
-          scope="public_profile,email,pages_messaging,pages_read_engagement,pages_show_list,pages_manage_metadata"
-          onResponse={this.handleResponse}
-          onError={this.handleError}
-        >
-          <span>Login via Facebook</span>
-        </Login>
-      </FacebookProvider>
-    );
-  
-  }
-}
+      }}
+    >
+      FacebookIntegration
+    </div>
+  );
+};
 
 export default App;
