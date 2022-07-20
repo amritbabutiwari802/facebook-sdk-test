a2c27622feaff3cbaab4eac49d3a14ae94ac560c
babuamrit <92739498+babuamrit@users.noreply.github.com>
2022-07-20 12:27:53 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useEffect } from "react";
 
 
 const App = (props) => {
+  const [permissionNotGranted, setPermissionStatus] = useState(false)
+  const [pageNotGranted, setPageStatus] = useState(false);
 
   useEffect(() => {
     window.fbAsyncInit = () => {
@@ -27,82 +29,100 @@ const App = (props) => {
     })(document, "script", "facebook-jssdk");
   }, []);
   return (
-    <div
-      onClick={() => {
-        window.FB.login(
-          function (response) {
-            if (response.authResponse) {
-              console.log("Welcome!  Fetching your information.... ", response);
-              window.FB.api(
-                `/${response.authResponse.userID}/permissions`,
-                "GET",
-                function (permissionResponse) {
-                  console.log("granted permission", permissionResponse);
-                  window.FB.api(
-                    `/${response.authResponse.userID}/accounts?fields=name&access_token=${response.authResponse.accessToken}`,
-                    "GET",
-                    async function (assigendPages) {
-                     
-                      console.log(assigendPages,"assigned pages");
-                      
-
-
-
-
-
-
-const backendResponse = await fetch(
-  "https://grambellbackend.loca.lt/integration",
-  {
-    method: "post",
-    headers: {
-      // firebase auth token
-      "x-client-id": "6aba4bef-456a-419b-b452-2b69dcd1c495",
-      "Content-Type": "application/json",
-      "Bypass-Tunnel-Reminder": "allow",
-    },
-    // body: JSON.stringify({ page, userId, accessToken }),
-    body: JSON.stringify({
-      provider: "FACEBOOK",
-      name: "Bibash Facebook",
-      config: {
-        pageId: assigendPages.data[0].id,
-        userId: response.authResponse.userID,
-        accessToken: response.authResponse.accessToken,
-      },
-    }),
-  }
-);
-const data = await backendResponse.json();
-console.log("data from backend", data);
-
-
-
-
-
-
-
-
-
-
-
-
+    <div>
+      {permissionNotGranted && (
+        <p>
+          you didn't frtant all permissions. Permissions are requier to proceed.
+          Click below to regrant permissions
+        </p>
+      )}
+      {pageNotGranted && (
+        <p>
+          you didn't frtant all page permissions. Page Permissions are requier to proceed.
+          Click below to regrant permissions
+        </p>
+      )}
+      <div
+        onClick={() => {
+         var breakOperation=false
+          window.FB.login(
+            function (response) {
+              if (response.authResponse) {
+                console.log(
+                  "Welcome!  Fetching your information.... ",
+                  response
+                );
+                window.FB.api(
+                  `/${response.authResponse.userID}/permissions`,
+                  "GET",
+                  function (permissionResponse) {
+                    console.log("granted permission", permissionResponse);
+                    for (let i = 0; i < permissionResponse.data.length; i++) {
+                      if (permissionResponse.data[i].status != "granted") {
+                        breakOperation= true
+                        setPermissionStatus(true);
+                        break;
+                      }
                     }
-                  );
-                }
-              );
-            } else {
-              console.log("User cancelled login or did not fully authorize.");
+                    if (breakOperation) {
+                    return 
+                  }
+                    window.FB.api(
+                      `/${response.authResponse.userID}/accounts?fields=name&access_token=${response.authResponse.accessToken}`,
+                      "GET",
+                      async function (assigendPages) {
+                        console.log(assigendPages, "assigned pages");
+                        if (assigendPages.data.length == 0)
+                        {
+                          breakOperation = true
+                          setPageStatus(true)
+                          }
+
+ if (breakOperation) {
+   return;
+ }
+                        const backendResponse = await fetch(
+                          "https://grambellbackend.loca.lt/integration",
+                          {
+                            method: "post",
+                            headers: {
+                              // firebase auth token
+                              "x-client-id":
+                                "6aba4bef-456a-419b-b452-2b69dcd1c495",
+                              "Content-Type": "application/json",
+                              "Bypass-Tunnel-Reminder": "allow",
+                            },
+                            // body: JSON.stringify({ page, userId, accessToken }),
+                            body: JSON.stringify({
+                              provider: "FACEBOOK",
+                              name: "Bibash Facebook",
+                              config: {
+                                pageId: assigendPages.data[0].id,
+                                userId: response.authResponse.userID,
+                                accessToken: response.authResponse.accessToken,
+                              },
+                            }),
+                          }
+                        );
+                        const data = await backendResponse.json();
+                        console.log("data from backend", data);
+                      }
+                    );
+                  }
+                );
+              } else {
+                console.log("User cancelled login or did not fully authorize.");
+              }
+            },
+            {
+              scope:
+                "public_profile,email,pages_messaging,pages_read_engagement,pages_show_list,pages_manage_metadata",
             }
-          },
-          {
-            scope:
-              "public_profile,email,pages_messaging,pages_read_engagement,pages_show_list,pages_manage_metadata",
-          }
-        );
-      }}
-    >
-      FacebookIntegr
+          );
+        }}
+      >
+        FacebookIntegr
+      </div>
     </div>
   );
 };
